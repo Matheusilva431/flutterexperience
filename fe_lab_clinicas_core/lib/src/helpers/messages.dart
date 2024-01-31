4396929ea2a9bd917f4590af0344d8dae6751928
Matheusilva431 <matheusilva431+github@gmail.com>
2024-01-31 02:29:04 -0300

    Configurado o sistema de mensagem com gerenciador de estado signal e criado uma ambiente para realizar teste

--- fe_lab_clinicas_core/lib/src/helpers/messages.dart
+++ fe_lab_clinicas_core/lib/src/helpers/messages.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
 final class Messages {
-  static void showErro(String message, BuildContext context) {
+  static void showError(String message, BuildContext context) {
     showTopSnackBar(
       Overlay.of(context),
       CustomSnackBar.error(message: message),
@@ -14,10 +15,66 @@ final class Messages {
     showTopSnackBar(
       Overlay.of(context),
       CustomSnackBar.info(message: message),
-    );}
+    );
+  }
+
   static void showSuccess(String message, BuildContext context) {
     showTopSnackBar(
       Overlay.of(context),
       CustomSnackBar.success(message: message),
-    );}
+    );
+  }
+}
+
+mixin MessageStateMixin {
+  final Signal<String?> _errorMessage = signal(null);
+  String? get errorMessage => _errorMessage();
+
+  final Signal<String?> _infoMessage = signal(null);
+  String? get infoMssage => _infoMessage();
+
+  final Signal<String?> _successMessage = signal(null);
+  String? get successMssage => _successMessage();
+
+  void clearError() => _errorMessage.value = null;
+  void clearInfo() => _infoMessage.value = null;
+  void clearSuccess() => _successMessage.value = null;
+
+  void showError(String message) {
+    untracked(() => clearError());
+    _errorMessage.value = message;
+  }
+
+  void showInfo(String message) {
+    untracked(() => clearInfo());
+    _infoMessage.value = message;
+  }
+
+  void showSuccess(String message) {
+    untracked(() => clearSuccess());
+    _successMessage.value = message;
+  }
+
+  void clearAllMessages() {
+    untracked(() {
+      clearError();
+      clearInfo();
+      clearSuccess();
+    });
+  }
+}
+
+mixin MessageViewMixin<T extends StatefulWidget> on State<T> {
+  void messageListener(MessageStateMixin state) {
+    effect(() {
+      switch (state) {
+        case MessageStateMixin(:final errorMessage?):
+          Messages.showError(errorMessage, context);
+        case MessageStateMixin(:final infoMssage?):
+          Messages.showInfo(infoMssage, context);
+        case MessageStateMixin(:final successMssage?):
+          Messages.showSuccess(successMssage, context);
+      }
+    });
+  }
 }
